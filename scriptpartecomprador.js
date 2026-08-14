2423f684b8e8b0c260d6de24f4974305a5283f0d
Sophia Azevedo <sophiaasescabora@gmail.com>
2026-08-14 18:39:34 -0300

    purchasing area

--- scriptpartecomprador.js
+++ scriptpartecomprador.js
@@ -1,1 +1,15 @@
+const comprador1 = document.getElementById("compralugar1");
+const comprador2 = document.getElementById("compralugar2");
 
+comprador1.addEventListener("click", IrParaCompra1);
+comprador2.addEventListener("click", IrParaCompra2);
+
+function IrParaCompra1() {
+   
+    window.location.href = "purchasing1.html";
+}
+
+function IrParaCompra2() {
+   
+    window.location.href = "purchasing2.html";
+}
